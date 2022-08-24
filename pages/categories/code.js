2a25417f26202e669a948221e259941233dec8c4
Duy Nguyễn <duykhanhchi1993@gmail.com>
2022-08-24 23:08:05 +0700

    fix bug for tags per page

--- pages/categories/code.js
+++ pages/categories/code.js
@@ -1,32 +1,46 @@
 import Layout from "../../components/layout";
 import Post from "../../components/post";
 import SideBar from "../../components/sidebar";
-import db from "../../config/db/index" 
-import BlogPost from "../../models/Post"
+import db from "../../config/db/index";
+import BlogPost from "../../models/Post";
 
-export default function Code({ posts }) {
+export default function Code({ posts, tags }) {
   function formatDate(date) {
     let res = new Date(date);
-    return res.getDate() + '/' + (Number(res.getMonth()) + 1).toString() + '/' + res.getFullYear();
+    return (
+      res.getDate() +
+      "/" +
+      (Number(res.getMonth()) + 1).toString() +
+      "/" +
+      res.getFullYear()
+    );
   }
   return (
     <div>
       <Layout active="code">
-        <h2 className="text-3xl font-mon font-bold mt-5 mb-14 sm:mb-10">Bài viết lập trình</h2>
+        <h2 className="text-3xl font-mon font-bold mt-5 mb-14 sm:mb-10">
+          Bài viết lập trình
+        </h2>
         <div className="grid grid-cols-3 gap-x-8 lg:grid-cols-none lg:gap-x-0">
-
           <div className="col-span-2 lg:order-2">
-          {(posts.map( (post) => <Post key={post._id} image = {post.thumbnail}
-          title={post.title}
-          description = "Chỉ với một số thẻ HTML thông dụng, bạn đã hoàn toàn có thể tạo riêng cho mình một trang web đơn giản."
-          time = {formatDate(post.createdAt)}
-          comment = "3 Bình luận"
-          link = {`/blogs/${post.slug}`}
-          tags = {post.tags} />)).reverse()}
+            {posts
+              .map((post) => (
+                <Post
+                  key={post._id}
+                  image={post.thumbnail}
+                  title={post.title}
+                  description="Chỉ với một số thẻ HTML thông dụng, bạn đã hoàn toàn có thể tạo riêng cho mình một trang web đơn giản."
+                  time={formatDate(post.createdAt)}
+                  comment="3 Bình luận"
+                  link={`/blogs/${post.slug}`}
+                  tags={post.tags}
+                />
+              ))
+              .reverse()}
           </div>
 
           <div className="">
-            <SideBar />
+            <SideBar tags={tags} />
           </div>
         </div>
       </Layout>
@@ -36,11 +50,18 @@ export default function Code({ posts }) {
 
 export async function getStaticProps() {
   await db.connect();
-  const posts = await BlogPost.find({categories: 'code'});
+  let posts = await BlogPost.find();
+  let tags = [];
+  posts = JSON.parse(JSON.stringify(posts));
+  posts.forEach((post) => {
+    post.tags.forEach((tag) => {
+      if (!tags.includes(tag)) tags.push(tag);
+    });
+  });
   return {
     props: {
-      posts: JSON.parse(JSON.stringify(posts)),
-      
+      posts,
+      tags,
     },
     revalidate: 10,
   };
--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,38 @@
 import Layout from "../components/layout";
 import Post from "../components/post";
 import SideBar from "../components/sidebar";
-import db from "../config/db/index" 
-import BlogPost from "../models/Post"
-import readTime from "../utils/read-time"
-import { formatDate } from "../utils/lib"
+import db from "../config/db/index";
+import BlogPost from "../models/Post";
+import readTime from "../utils/read-time";
+import { formatDate } from "../utils/lib";
 
 export default function Home({ posts, tags }) {
   return (
     <div>
       <Layout active="home">
-        <h2 className="text-3xl font-mon font-bold mt-5 mb-14 sm:mb-10">Tổng hợp bài viết</h2>
+        <h2 className="text-3xl font-mon font-bold mt-5 mb-14 sm:mb-10">
+          Tổng hợp bài viết
+        </h2>
         <div className="grid grid-cols-3 gap-x-8 lg:grid-cols-none lg:gap-x-0">
-
           <div className="col-span-2 lg:order-2">
-          {(posts.map( (post) => <Post key={post._id} image = {post.thumbnail}
-          title={post.title}
-          description = {post.description}
-          time = {formatDate(post.createdAt)}
-          read_duration = {`${readTime(post.content)} phút đọc`}
-          link = {`/blogs/${post.slug}`}
-          tags = {post.tags} />)).reverse()}
+            {posts
+              .map((post) => (
+                <Post
+                  key={post._id}
+                  image={post.thumbnail}
+                  title={post.title}
+                  description={post.description}
+                  time={formatDate(post.createdAt)}
+                  read_duration={`${readTime(post.content)} phút đọc`}
+                  link={`/blogs/${post.slug}`}
+                  tags={post.tags}
+                />
+              ))
+              .reverse()}
           </div>
 
           <div className="">
-            <SideBar tags={tags}/>
+            <SideBar tags={tags} />
           </div>
         </div>
       </Layout>
@@ -37,11 +45,11 @@ export async function getStaticProps() {
   let posts = await BlogPost.find();
   let tags = [];
   posts = JSON.parse(JSON.stringify(posts));
-  posts.forEach( (post) => {
-    post.tags.forEach( (tag) => {
-      if(!tags.includes(tag)) tags.push(tag);
-    })
-  })
+  posts.forEach((post) => {
+    post.tags.forEach((tag) => {
+      if (!tags.includes(tag)) tags.push(tag);
+    });
+  });
   return {
     props: {
       posts,
